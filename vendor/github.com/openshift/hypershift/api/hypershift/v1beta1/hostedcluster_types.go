e4c7e5ec9d7cb696d1c2b6260104b463f1b5aa36
Peter Hunt <pehunt@redhat.com>
2024-10-28 15:52:28 -0400

    use NodeSpec in ClusterConfiguration
    Signed-off-by: Peter Hunt <pehunt@redhat.com>

--- vendor/github.com/openshift/hypershift/api/hypershift/v1beta1/hostedcluster_types.go
+++ vendor/github.com/openshift/hypershift/api/hypershift/v1beta1/hostedcluster_types.go
@@ -1544,6 +1544,10 @@ type ClusterConfiguration struct {
 	// +rollout
 	// +optional
 	Proxy *configv1.ProxySpec `json:"proxy,omitempty"`
+
+	// NodeSpec holds cluster-wide details for the node configuration object.
+	// +optional
+	Node *configv1.NodeSpec `json:"node,omitemtpy"`
 }
 
 // +genclient
